6dedb2e350c7802dccdeccc66e981f0bd33ee75c
Prerna417 <b421036@iiit-bh.ac.in>
2026-05-06 17:35:28 +0530

    fixed cookie error

--- server/utils/index.js
+++ server/utils/index.js
@@ -20,8 +20,8 @@ export const createJWT = (res, userId) => {
     // Change sameSite from strict to none when you deploy your app
     res.cookie("token", token, {
         httpOnly: true,
-        secure: process.env.NODE_ENV !== "development",
-        sameSite: "strict", //prevent CSRF attack
+        secure: true,
+        sameSite: "none", //prevent CSRF attack
         maxAge: 1 * 24 * 60 * 60 * 1000, //1 day
     })
 }
